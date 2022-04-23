0934caac1c406b1692f0c26b5c9b91b736957103
Hazel <57199537+qthazel@users.noreply.github.com>
2022-04-23 01:20:18 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'QTApp'
+copyright = '2022, Hazel Lynn'
+author = 'iamhazel'
 
 release = '0.1'
 version = '0.1.0'
